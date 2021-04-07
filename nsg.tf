711e107f51b046bd6110419fecb404bd64f23f33
rakall <rakall@hotmail.es>
2021-04-07 10:19:21 +0200

    Create nsg.tf

---
+++ nsg.tf
@@ -0,0 +1,32 @@
+resource "azurerm_resource_group" "rg-nsg" {
+  name = "rgt-nsg"
+  location = var.loc
+}
+
+resource "azurerm_network_security_group" "nsg" {
+  name                = "nsg-acceso"
+  location = var.loc
+  resource_group_name = azurerm_resource_group.rg-nsg.name
+
+   security_rule {
+    name                       = "ReglaAcceso"
+    priority                   = 102
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "*"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  tags = {
+    environment = "Labs"
+  }
+}
+output "gruposeguridad" {
+  value = azurerm_network_security_group.nsg.id
+}
+output "rgnsg" {
+  value = azurerm_resource_group.rg-nsg.name
+}
