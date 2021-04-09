3d483c1ed5345d902955130d89a913f67b3a23d3
rakall <rakall@hotmail.es>
2021-04-09 14:59:36 +0200

    Update lb.tf

--- lb.tf
+++ lb.tf
@@ -3,7 +3,10 @@ resource "azurerm_lb" "elb" {
   resource_group_name = var.rg
   location            = var.loc
   sku                 = "Standard"
-
+  frontend_ip_configuration {
+    name                 = "PublicIPAddress"
+    public_ip_address_id = azurerm_public_ip.ippublica.id
+  }
 }
 resource "azurerm_public_ip" "ippublica" {
   name                = "ippublicabalanceador"
