3ec069deffa03d35c7e279129ff6c41d55d86dc9
rakall <rakall@hotmail.es>
2021-04-12 10:28:26 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -38,9 +38,18 @@ resource "azurerm_lb_rule" "example" {
   frontend_ip_configuration_name = "PublicIPAddress"
 }
 
+  frontend_ip_configuration {
+    name                 = "PublicIPAddress"
+    public_ip_address_id = azurerm_public_ip.example.id
+  }
+}
 
-
-
+resource "azurerm_public_ip" "example" {
+  name                = "PublicIPForLB"
+  location            = var.loc
+  resource_group_name = "terraform"
+  allocation_method   = "Static"
+}
 
 
 
